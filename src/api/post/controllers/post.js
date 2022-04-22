e4c57d28674827e631a122af2f24de5159da7593
Ailton Duarte <adidas.coyas@gmail.com>
2022-04-22 12:18:13 -0100

    change findOne to use slug

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -1,9 +1,35 @@
-'use strict';
+"use strict";
 
 /**
  *  post controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController("api::post.post", ({ strapi }) => ({
+  // Method 3: Replacing a core action
+  // async findOne(ctx) {
+  //   const { id } = ctx.params;
+  //   const { query } = ctx;
+
+  //   console.log(id);
+
+  //   const entity = await strapi.service("api::post.post").findOne(id, query);
+  //   const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+  //   return this.transformResponse(sanitizedEntity);
+  // },
+  async findOne(ctx) {
+    const { id: slug } = ctx.params;
+    const { query } = ctx;
+
+    if (!query.filters) query.filters = {};
+    query.filters.slug = { $eq: slug };
+
+    const entity = await strapi.service("api::post.post").find(query);
+
+    const { results } = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(results[0]);
+  },
+}));
